6053b03ca8fc7a8731645d0fd52bbea3b014ee22
JRodDvlpr <Mialienproductions@gmail.com>
2020-05-23 12:06:31 -0400

    :bug: fixed for quieries

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@
 -- Find all customers with postal code 1010
 
     SELECT *
-    FROM [Customers]]
+    FROM [Customers]
     WHERE [PostalCode] = "1010";
 
 -- Find the phone number for the supplier with the id 11
@@ -15,16 +15,16 @@
 -- List first 10 orders placed, sorted descending by the order date
 
     SELECT *
-    FROM [Orders]
-    ORDER BY [OrderDate] DESC
-    LIMIT 10;
+    FROM Orders
+    ORDER BY OrderDate DESC
+    LIMIT 10
 
 -- Find all customers that live in London, Madrid, or Brazil
 
     SELECT *
-    FROM [Customers]
-    WHERE [City] = 'London' OR [City] ='Madrid' OR [Country] = 'Brazil'
-    ORDER BY [OrderDate] DESC
+    FROM Customers
+    WHERE City = 'London' OR [City] ='Madrid' OR [Country] = 'Brazil'
+    
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
